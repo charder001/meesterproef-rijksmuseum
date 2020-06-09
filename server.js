9ddd5cef13c6cd06fc3de82f63ce717564c97a6d
Charder001 <rick.chardet@hva.nl>
2020-06-09 13:30:41 +0200

    added chrono

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+var chrono = require('chrono-node');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -86,5 +87,35 @@ app.post('/selectedDate', (req, res) => {
     }) 
     
 })
+
+
+//chrono
+
+var morgenParser = new chrono.Parser();
+
+// Provide search pattern
+morgenParser.pattern = function () { return /morgen/; };
+
+// This function will be called when matched pattern is found
+morgenParser.extract = function(text, ref, match, opt) { 
+    
+    // Return a parsed result, that is 25 December
+    return new chrono.ParsedResult({
+        ref: chrono.parseDate('tomorrow'),
+        text: match[0],
+        index: match.index,
+    });
+};
+
+
+
+var custom = new chrono.Chrono();
+custom.parsers.push(morgenParser);
+
+console.log(custom.parseDate("morgen"))
+
+// var pickedDate = chrono.parseDate('morgen')
+// console.log(pickedDate)
+
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
