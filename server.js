6e17905dda17d58aed1eac88d657db9140080ee3
Charder001 <rick.chardet@hva.nl>
2020-06-10 10:26:43 +0200

    added chronojs to testpage

--- server.js
+++ server.js
@@ -19,28 +19,6 @@ app.set('views', 'views');
 
 require('./routes')(app);
 
-//chrono
-
-var morgenParser = new chrono.Parser();
-
-// Provide search pattern
-morgenParser.pattern = function () { return /morgen/; };
-
-// This function will be called when matched pattern is found
-morgenParser.extract = function(text, ref, match, opt) { 
-    
-    // Return a parsed result, that is 25 December
-    return new chrono.ParsedResult({
-        ref: chrono.parseDate('tomorrow'),
-        text: match[0],
-        index: match.index,
-    });
-};
-
-var custom = new chrono.Chrono();
-custom.parsers.push(morgenParser);
-
-console.log(custom.parseDate("morgen"))
 
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
