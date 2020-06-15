b9a1011237c6eaf0cd0400b87b0d2836a23115f9
LarsBreuren <larsbreuren@gmail.com>
2020-06-15 15:12:15 +0200

    fixed checkbox bug on ticket page

--- public/js/ticket.js
+++ public/js/ticket.js
@@ -69,18 +69,9 @@ function selectNext(){
  //   console.log(localStorage.getItem("ticketAmount"));
 }
 
-
-//   let myObj = {
-//       ticketAmount: 5
-//   }
-
-//   let myObj_serialized = JSON.stringify(myObj);
-//   localStorage.setItem("myObj", myObj_serialized);
-
-//   let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
-
-//   console.log(myObj_deserialized);
-
-
-
-// localStorage.clear();
+discount.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("discountTickets").click();
+    }
+    });
