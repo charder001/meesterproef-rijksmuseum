09e9ca7823a52373a2ec4a5bbde37f7ed2c89bc7
LarsBreuren <larsbreuren@gmail.com>
2020-06-15 12:36:14 +0200

    New ticket selection layout

--- public/js/ticket.js
+++ public/js/ticket.js
@@ -1,25 +1,19 @@
-let standard = document.getElementById("standardTickets");
 let discount = document.getElementById("discountTickets");
 let standardContent = document.getElementById("content1");
 let DiscountContent = document.getElementById("content2");
 
-
-standard.addEventListener("click", showStandardTickets); 
 discount.addEventListener("click", showDiscountTickets); 
 
 
 function showStandardTickets(){
-    content1.classList.add("active");
-    content2.classList.remove("active");
+    content1.classList.toggle("active");
 
     // standard.classList.add("headerActive");
     // discount.classList.remove("headerActive");
 }
 
 function showDiscountTickets(){
-    content2.classList.add("active");
-    content1.classList.remove("active");
-
+    content2.classList.toggle("active");
     // discount.classList.add("headerActive");
     // standard.classList.remove("headerActive");
 }
