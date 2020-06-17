0a60b30353e4387ae039727717bbd1ba3d27bc48
Charder001 <rick.chardet@hva.nl>
2020-06-17 21:36:17 +0200

    fixed rembrandt flow

--- public/js/test.js
+++ public/js/test.js
@@ -1,4 +1,18 @@
-window.location.hash = '#selectedDay';
+
+
+document.getElementById("flexDateSubmit").addEventListener("click", handler);
+
+// handler function
+function handler(e) {
+	// remove this handler
+	e.target.removeEventListener(e.type, arguments.callee);
+    e.preventDefault()
+    document.getElementById("flexDateForm").insertAdjacentHTML("beforeend", "<a href='#' id='unavailable'>Deze datum is niet beschikbaar klik hier voor de eerstvolgende beschikbare datum: morgen</a>")
+    document.getElementById("unavailable").addEventListener("click", function(){
+        document.getElementById("flexDateField").value = "morgen"
+        document.getElementById("flexDateSubmit").click();
+    })
+}
 
 
 document.getElementById("flexDateField").addEventListener("keydown", function(event) {
